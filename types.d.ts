166f3d493fbfbe7a97e69a027c37ea666b58bdf5
Robert Manolea <manolea.robert@gmail.com>
2019-02-09 14:52:13 +0100

    Added TS typings

---
+++ types.d.ts
@@ -0,0 +1,23 @@
+declare class LCUConnector {
+        constructor(path: string);
+
+        static getLCUPathFromProcess(): Promise<string | void>;
+
+        static isValidLCUPath(dirPath: string): boolean;
+
+        start(): void;
+
+        stop(): void;
+
+        on(event: 'connect', listener: (data: {
+            address: string,
+            port: number,
+            username: string,
+            password: string,
+            protocol: string
+        }) => void): this;
+
+        on(event: 'disconnect', listener: () => void): this;
+}
+
+export = LCUConnector;
